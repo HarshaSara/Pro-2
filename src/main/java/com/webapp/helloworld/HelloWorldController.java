c19217d1b8c8a61ebff4989ed3755a8a2ab311b3
sneha <snehatoms91@gmail.com>
2024-05-15 12:45:49 -0400

    Proj4 java codetaken from Proj 2

---
+++ src/main/java/com/webapp/helloworld/HelloWorldController.java
@@ -0,0 +1,26 @@
+package com.webapp.helloworld;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+
+	@GetMapping("/")
+	public String helloWorld2() {
+		return "hello world this is app services with java..................!";
+	} 
+
+	@GetMapping("/abc")
+	public String helloWorld3() {
+		return "hello world this is app services with jav..................!";
+	} 
+	@GetMapping("/hello-world")
+	public String helloWorld() {
+		return "hello world..................!";
+	} 
+	@GetMapping("/hello")
+	public String helloWorld1() {
+		return "hello world this is java application..................!";
+	} 
+}
